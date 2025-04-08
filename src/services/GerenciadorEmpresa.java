780d2eb2912c058b99c54a0868e66f1e3f1f2be7
Marlon Ferreira <mferreirab03@gmail.com>
2025-04-08 11:38:25 -0300

    feat: Novos metodos(procurarEmpresa,removerEmpresa exibirVagas)

--- src/services/GerenciadorEmpresa.java
+++ src/services/GerenciadorEmpresa.java
@@ -20,5 +20,44 @@ public class GerenciadorEmpresa {
         System.out.println("Cadastro concluido");
         return  true;
     }
+
+    public Object procurarEmpresa(String name){
+        for(Empresa e : listaEmpresa){
+            if(e.getName().equals(name)){
+                System.out.println("Empresa e encontrada");
+                System.out.println(e);
+                return e;
+            }
+        }
+        System.out.println("Empresa n foi encontrada");
+        return null;
+    }
+    public boolean removerEmpresa(String name, String motivo){
+        if(listaEmpresa != null && listaEmpresa.size()> 0){
+            for(Empresa e : listaEmpresa){
+                if(e.getName().equals(name)){
+                    listaEmpresa.remove(e);
+                    if(motivo != null){
+                        System.out.println("Motivo: " + motivo);
+                        return true;
+                    }
+                    else{
+                        listaEmpresa.remove(e);
+                        System.out.println("Motivo: não informado");
+                        return false;
+                    }
+                }
+            }
+        }
+        System.out.println("não tem vagas para remover");
+        return false;
+    }
+
+    public void exibirVagas(){
+        for(Empresa e: listaEmpresa){
+            System.out.println(e);
+
+        }
+    }
 }
 
